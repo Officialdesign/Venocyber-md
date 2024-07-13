1ae9905f29dfbf1a70c763bc5b345146a0f6ae44
Officialdesign <153008231+Officialdesign@users.noreply.github.com>
2024-07-13 17:35:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VagzM5RGk
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/12b0fb245e6b8ba4bf718.jpg";
 global.devs = "255742862266";
 global.sudo = process.env.SUDO || "255742862266";
-global.owner = process.env.OWNER_NUMBER || "255742862266";
+global.owner = process.env.OWNER_NUMBER || "2347085803941";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "true";
 global.wlcm = process.env.WELCOME || "true";
@@ -42,7 +42,7 @@ global.scan = "https://rb.gy/3keql8";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUUROLzZxL0VBY0RFNitmcy9DWUtaa2dEZDh5R1RqeSt6NU5sUlR1VGJsWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiREFwTEJvbG53ZlNwcXZ4UlpNdDBzekhoZTJxdE5NSHpuU2FWUXlnNFRUOD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI2Rk8rZ01HQzFyOGEzVU1nR01ZTVBac3FWYkxOMEQzS1VHNlNFUVg1dFZVPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI5emJaWFhUT084eHZiRkJIMDZSUHNIalFRa0VTQUJxSGxKVXFKN1dPRnhZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InNLRzdVUjBNU1RVWHlNVXZGeEZ6VXJJRkhERkxqanc3SkMraWhjeFFzVzQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im80SkNtUHFVUjJ6THVTZG9ORFp1ak41MFltekJIbG9GZkZnMVZsa20wRmc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMkZ5T2NrRUU5MFJJcTAxejR0N2Z1eDFyUWpldnphK1NvVW1LOEsxbXZHcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWHFNWnc3MkhPWkI2YnpkcXk0cGt1azVCY3BCNGMwNWxoZjZ4QUlOcUx5QT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJIbXEyQlB1VTkzL29FKy9qMEtKbFRxVE5BTmxHdUdlenhMbHppaXpvazZ5QXA0SXQ5VnNMZkVrV0RFL0NVRk1ZM1l2VkpkZGVoZU14OUt0UzRoeGp3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTcwLCJhZHZTZWNyZXRLZXkiOiJyWTd4dnIvRDh6UktreUk4Z1ZkMDhrbjVYZXpaUldBdXJjd25OekY3cVRJPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJ6SFVQYmxuYlJIZXRoaERLc2IwTGp3IiwicGhvbmVJZCI6ImQyZjIyN2FmLTM1MTgtNDNlZC05MTUwLWNmOTA1NTcwM2FjOSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJoTUtyTkhxcHpaOTlnaERpTU4wSDJtWkdLTkk9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiak5Cc28wdURBTHdwN3pSUTQ2NEtQWTNOUis0PSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjZMV1E0M0pBIiwibWUiOnsiaWQiOiIyMzQ3MDg1ODAzOTQxOjdAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0oyV2dKc0hFTHJXeXJRR0dBSWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6ImYyOGhqVlZFSVJDRVpobTNONjdXMzVLUjFwbjBlT1BaZWlkK1EwVCtQRjg9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlJzOGpMNUxHVG5EVkZOY0FVV1lSeWhObUdYeEJLNWk0cGpsKzNzeC8xUzEwRlVSL1J6QmNpTUY4K1FZZFdTYzkwdWYraGFDVnRkb2xXTG1OWW9tUEJBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJrQnlRazR2djhmS091V01aZTA1MmNyR1JMSzd3cCtnalpxQXlkODVwUVdqNnVvVEFEaGk0dndqeFNDOGVNbDRCMWN5RWpNcDJHT0tLZGpCSThkaDhoUT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzNDcwODU4MDM5NDE6N0BzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJYOXZJWTFWUkNFUWhHWVp0emV1MXQrU2tkYVo5SGpqMlhvbmZrTkUvanhmIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIwODg4MTM2fQ=="
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ".",
@@ -51,8 +51,8 @@ module.exports = {
   caption: process.env.CAPTION || "you're using first powerful bot to be released in Tanzania| enjoy your time| this is 𝐕𝐄𝐍𝐎𝐂𝐘𝐁𝐄𝐑 𝐌𝐃",
   author: process.env.PACK_AUTHER || "VENOCYBER",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "VENOCYBER-𝐌𝐃",
-  ownername: process.env.OWNER_NAME || "VENOCYBER",
+  botname: process.env.BOT_NAME || "OFFIXIAL-𝐌𝐃",
+  ownername: process.env.OWNER_NAME || "Offixial Design",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
